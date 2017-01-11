2635f12430bd7462036cabed41dcdadd2e4a7b93
dilltsao <dill0819@gmail.com>
2017-01-11 23:48:33 +0800

    First Commit

---
+++ Day1Homework_TDD/GroupSum.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1Homework_TDD
+{
+    public class GroupSum
+    {
+        public static List<int> getSumByGroup(List<IProductInfo> enumrableObj, int groupNum, string field)
+        {
+            if (groupNum <= 0)            
+                throw new ArgumentOutOfRangeException("Negative number of groupNum is illegal.");            
+
+            List<int> results = new List<int>();
+            int decresedCount = enumrableObj.Count();
+            int flag = 0;
+            while(decresedCount > 0)
+            {
+                int sum = 0;
+                
+                int i;
+                for(i = 0; i < groupNum; i++)
+                {
+                    if (flag + i >= enumrableObj.Count())
+                        break;
+
+                    IProductInfo productInfo = enumrableObj.ElementAt(flag + i);
+                    sum += productInfo.getValue(field);
+                }
+
+                flag += groupNum;//0 -> 3,6,9                
+                decresedCount -= groupNum;//11 -> 8,5,2
+
+                results.Add(sum);
+            }
+
+            //var sum = products.Take(take).Sum(product => product.pluck(attr));
+            //var groupSum = enumrableObj.Take(groupNum).Sum();
+
+            return results;
+        }
+    }
+
+    public interface IProductInfo
+    {
+        //bool AddField(string fieldname);
+        int ID { get; set; }
+        int Cost { get; set; }
+        int Revenue { get; set; }
+        int SellPrice { get; set; }
+        int getValue(string fieldName);
+    }
+    
+    public class ProductInfo: IProductInfo
+    {
+        public int ID { get; set; }
+        public int Cost { get; set; }
+        public int Revenue { get; set; }
+        public int SellPrice { get; set; }  
+      
+        public ProductInfo(int id, int cost, int revenue, int sellprice)
+        {
+            ID = id;
+            Cost = cost;
+            Revenue = revenue;
+            SellPrice = sellprice;
+        }
+
+        public int getValue(string fieldName)
+        {
+            int output;
+            switch(fieldName.ToUpper())
+            {
+                case "ID":
+                    output = this.ID;
+                    break;
+                case "COST":
+                    output = this.Cost;
+                    break;
+                case "REVENUE":
+                    output = this.Revenue;
+                    break;
+                case "SELLPRICE":
+                    output = this.SellPrice;
+                    break;
+                default:
+                    output = 0;
+                    break;
+            }
+            return output;
+        }
+    }
+}
---
+++ Day1Homework_TDD_Test/Day1Homework_TDD_Test.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day1Homework_TDD;
+using System.Collections.Generic;
+
+namespace Day1Homework_TDD_Test
+{
+    [TestClass]
+    public class Day1Homework_TDD_Test
+    {
+        List<IProductInfo> products = null;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            products = new List<IProductInfo>()
+            {
+                new ProductInfo(1, 1, 11, 21),
+                new ProductInfo(2, 2, 12, 22),
+                new ProductInfo(3, 3, 13, 23),
+                new ProductInfo(4, 4, 14, 24),
+                new ProductInfo(5, 5, 15, 25),
+                new ProductInfo(6, 6, 16, 26),
+                new ProductInfo(7, 7, 17, 27),
+                new ProductInfo(8, 8, 18, 28),
+                new ProductInfo(9, 9, 19, 29),
+                new ProductInfo(10, 10, 20, 30),
+                new ProductInfo(10, 11, 21, 31)
+            };
+
+        }
+
+        [TestCleanup]
+        public void TestCleanUp()
+        {
+            products.Clear();
+        }
+
+        [TestMethod]
+        public void 三個成員為一組_每組取Cost總和_結果為4組分別為_6_15_24_21()
+        {
+            //arrange
+            var expected = new List<int> { 6, 15, 24, 21 };            
+
+            //act
+            var actual = GroupSum.getSumByGroup(products, 3, "cost");
+
+            //assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void 四個成員為一組_每組取Revenue總和_結果為3組分別為_50_66_60()
+        {
+            //arrange
+            var expected = new List<int> { 50, 66, 60 };
+
+            //act
+            var actual = GroupSum.getSumByGroup(products, 4, "revenue");
+
+            //assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void 數字超過集合成員數量為一組_每組取Cost總和_結果視為1組並為_66()
+        {
+            //arrange
+            var expected = new List<int> { 66 };
+
+            //act
+            var actual = GroupSum.getSumByGroup(products, 100, "cost");
+
+            //assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void 負數值的成員為一組_將拋出例外_ArgumentOutOfRangeException()
+        {
+            //arrange
+            var expected = new List<int> { };
+
+            //act
+            var actual = GroupSum.getSumByGroup(products, -1, "cost");
+
+            //assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
